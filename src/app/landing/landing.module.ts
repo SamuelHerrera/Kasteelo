fee11051ccde5aa134bec5511a8de4338fca51d9
Jesus Samuel Herrera Fuente <samuel.herrera@4thsource.com>
2018-05-10 16:46:32 -0500

    WIP HEADER for landing page

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -6,12 +6,14 @@ import { LandingComponent } from './landing.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { QuotationComponent } from './components/quotation/quotation.component';
 import { MainComponent } from './components/main/main.component';
+import { EventsComponent } from './components/events/events.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LandingRoutingModule
   ],
-  declarations: [HeaderComponent, LandingComponent, GalleryComponent, QuotationComponent, MainComponent]
+  declarations: [HeaderComponent, LandingComponent, GalleryComponent, QuotationComponent, MainComponent, EventsComponent, ContactComponent]
 })
 export class LandingModule { }
--- src/app/landing/router/landing-routing.module.ts
+++ src/app/landing/router/landing-routing.module.ts
@@ -4,6 +4,8 @@ import { LandingComponent } from '../landing.component';
 import { GalleryComponent } from '../components/gallery/gallery.component';
 import { QuotationComponent } from '../components/quotation/quotation.component';
 import { MainComponent } from '../components/main/main.component';
+import { EventsComponent } from '../components/events/events.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -13,7 +15,9 @@ const routes: Routes = [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
       { path: 'main', component: MainComponent },
       { path: 'gallery', component: GalleryComponent },
-      { path: 'quotation', component: QuotationComponent }
+      { path: 'quotation', component: QuotationComponent },
+      { path: 'events', component: EventsComponent },
+      { path: 'contact', component: ContactComponent }
     ]
   },
   { path: '**', redirectTo: '', }
