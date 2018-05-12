a65ef7a59243455aeadd299eb492ceeafdfeb249
Jesus Samuel Herrera Fuente <samuel.herrera@4thsource.com>
2018-05-12 00:20:00 -0500

    WIP Gallery

--- src/app/landing/components/gallery/gallery.component.ts
+++ src/app/landing/components/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var document: any;
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  ambientacion: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.loadScript().then(data => {
+      console.log('script loaded ', data);
+    }).catch(error => console.log(error));
+  }
+
+  loadScript() {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = '../../../../assets/js/carousel.js';
+      if (script.readyState) {
+        script.onreadystatechange = () => {
+          if (script.readyState === 'loaded' || script.readyState === 'complete') {
+            script.onreadystatechange = null;
+            resolve({ script: name, loaded: true, status: 'Loaded' });
+          }
+        };
+      } else {
+        script.onload = () => {
+          resolve({ script: name, loaded: true, status: 'Loaded' });
+        };
+      }
+      script.onerror = (error: any) => resolve({ script: name, loaded: false, status: 'Loaded' });
+      document.getElementsByTagName('head')[0].appendChild(script);
+
+    });
   }
 
 }
